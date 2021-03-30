46325480deb8dbd60f20a956095005fa99011799
garethcarvalho <gcarv023@mtroyal.ca>
2021-03-29 20:12:20 -0600

    Created config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'stocks');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
+?>
