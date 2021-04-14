a4647f8d259e4533630229b8e548af4b92f42057
Nuwasir Arham <narha672@mtroyal.ca>
2021-04-14 08:35:30 -0600

    created profile page

---
+++ profile.php
@@ -0,0 +1,5 @@
+<?php
+    echo "<h1>This page is currently under contruction. We will be back soon!</h1>";
+    echo "</br></br>";
+    echo "<h1><a href= 'index.php' class= 'button-label'>Go Back</a></h1>";
+?>
