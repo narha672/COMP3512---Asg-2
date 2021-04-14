b2631ff4827444b6e27935ee57f89c75124cd2f0
Zaid Khusro <43654642+zkhus567@users.noreply.github.com>
2021-04-13 23:40:55 -0600

    Started and Finished the about page
    Finished it all nothing to be changed dont touch it now its good

--- about.php
+++ about.php
@@ -20,7 +20,41 @@
             <span><a>Credits</a></span>
         </div>
         <main>
-        </br></br></br></br></br></br></br>
+        </br></br></br></br></br>
+        <section>
+            <h1 style="padding:10px;">Description</h1>
+            <p style="padding: 10px;">Welcome to the website created for Assignmnet 2 for COMP 3512 (Web 2) class at Mount Royal University where 
+                you can view all of your favorite companies and explore the different ways you can take a look at the overall financials and add cpmpanies to your own Portfolio
+                so you do not miss any updates! 
+                </br></br>
+                This course is offered by Randy Connolly and it is a third-year course for Computer information
+                Systems students which focuses on JavaScript and PHP.
+            </p>
+        </section>
+        <section>
+            <h1 style="padding:10px;">Technologies Used</h1>
+            <div>
+                <ul>
+                    <li><a href="https://loading.io/">Loading.io</a></li>
+                    <li><a href="https://www.w3schools.com/">w3schools.com</a></li>
+                </ul>
+            </div>
+        </section>
+        <section>
+            <h1 style="padding:10px;">GitHub Links</h1>
+            <div>
+                <ul>
+                    <li><a href="https://github.com/narha672/COMP3512---Asg-2">Assignment 2 Repository</a></li>
+                    <li><a href="https://github.com/garethcarvalho">Gareth Carvalho</a></li>
+                    <li><a href="https://github.com/narha672">Nuwasir Arham</a></li>
+                    <li><a href="https://github.com/zkhus567">Zaid Khusro</a></li>
+                </ul>
+            </div>
+        </section>
+
+
+            
+        
 
         </main>
     </body>
